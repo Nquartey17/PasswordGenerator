9be3b0cb22e70ccae67c4c70ecef7d1796c0427f
Nquartey17 <niikwarteiq@gmail.com>
2023-12-06 09:17:50 -0500

    Loop to check each character updated

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,17 @@ import java.util.*;
 public class Main {
 
     /* I'll try using a scoring system to determine password strength
-    0-1 points = Weak
-    2-4 points = Medium
-    5-8 points = Good
-    9+ points = Strong
+    0 points = Weak
+    1-2 points = Medium
+    3-5 points = Good
+    6+ points = Strong
      */
-    public void passwordStrength(String password) {
+    public static void passwordStrength(String password) {
 
+    }
+
+    public static void badPassword(String message) {
+        System.out.println(message + " Please try again");
     }
 
     public static void main(String[] args) {
@@ -28,19 +32,49 @@ public class Main {
         System.out.println("Enter your password");
         String password = scnr.nextLine();
         boolean accepted = false;
-        int uppercase, lowercase, number, specialChar = 0;
+        int uppercase = 0, lowercase = 0, number = 0, specialChar = 0;
 
+        //Check every character in string and add to values for requirements are met
+        //Remember to reset values after each failure
+        for (int i = 0; i < password.length(); i++) {
+            char letter = password.charAt(i);
+            if (Character.isDigit(letter)) {
+                number++;
+            } else if (Character.isUpperCase(letter)) {
+                uppercase++;
+            } else if (Character.isLowerCase(letter)) {
+                lowercase++;
+            } else {
+                specialChar++;
+            }
+        }
+
+        //create a method to prevent repetition
         while (!accepted) {
             if (password.length() < 8) {
-                System.out.println("Password length is too short. Please try again");
+                badPassword("Password length is too short.");
                 password = scnr.nextLine();
-            }
-            else {
+            } else if (number < 1) {
+                badPassword("Password must contain numeric value.");
+                password = scnr.nextLine();
+            } else if (uppercase < 1) {
+                badPassword("Password must contain uppercase letter.");
+                password = scnr.nextLine();
+            } else if (lowercase < 1) {
+                badPassword("Password must contain lowercase letter.");
+                password = scnr.nextLine();
+            } else if (specialChar < 1) {
+                badPassword("Password must contain special character.");
+                password = scnr.nextLine();
+            } else {
                 System.out.println("Password accepted!");
                 accepted = true;
             }
         }
+
     }
+
+
 
 
 }
