55ea209883c2bcf0120c232e89a8fcc8e6a9fedf
Nquartey17 <niikwarteiq@gmail.com>
2023-12-06 08:07:35 -0500

    Minor adjustments and password strength method started

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
 import java.util.*;
 
+
 public class Main {
+
+    /* I'll try using a scoring system to determine password strength
+    0-1 points = Weak
+    2-4 points = Medium
+    5-8 points = Good
+    9+ points = Strong
+     */
+    public void passwordStrength(String password) {
+
+    }
+
     public static void main(String[] args) {
+
         Scanner scnr = new Scanner(System.in);
         System.out.println("Welcome to the password generator program");
         System.out.println("Enter a password and the program will determine the strength of the password\n\n" +
@@ -15,9 +28,19 @@ public class Main {
         System.out.println("Enter your password");
         String password = scnr.nextLine();
         boolean accepted = false;
+        int uppercase, lowercase, number, specialChar = 0;
 
-        if (password.length() < 8) {
-            System.out.println("Password length is too short. Please try again");
+        while (!accepted) {
+            if (password.length() < 8) {
+                System.out.println("Password length is too short. Please try again");
+                password = scnr.nextLine();
+            }
+            else {
+                System.out.println("Password accepted!");
+                accepted = true;
+            }
         }
     }
+
+
 }
