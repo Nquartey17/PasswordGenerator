ae426fca2d152c70ef9f9dd3904679605b583407
Nquartey17 <niikwarteiq@gmail.com>
2023-12-11 13:03:01 -0500

    Random generator working, I will simplify repeating code later

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,88 @@ import java.util.*;
 public class Main {
 
     /* I'll try using a scoring system to determine password strength
-    0 points = Weak
-    1-2 points = Medium
-    3-5 points = Good
-    6+ points = Strong
+    0-1 points = Weak
+    2-3 points = Medium
+    4-6 points = Good
+    7+ points = Strong
+    ^ Original method won't work, try thinking of something else
      */
-    public static void passwordStrength(String password) {
+    public static void passwordStrength(int numCount, int upCount, int lowCount, int spCount) {
+        String text = "Password Strength is ";
+        int total = (numCount + upCount + lowCount + spCount) / 4;
+        if (total <= 1) {
+            System.out.println(text + "weak");
+        } else if (total == 2 || total == 3) {
+            System.out.println(text + "medium");
+        } else if (total == 4 || total == 5 || total == 6) {
+            System.out.println(text + "good");
+        } else {
+            System.out.println(text + "strong");
+        }
 
     }
 
+    /* Password length will always be 14 characters
+    contains 3 numbers, 3 special characters, 3 uppercase letters, and 5 lowercase
+    characters will be randomly scrambled, no duplicates
+    I will simplify repetitive code later
+     */
     public static String generatePassword() {
-        return null;
+        HashSet<Character> charSet = new HashSet<>();
+        Random rand = new Random();
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        char[] nums = "0123456789".toCharArray();
+        char[] spChars = "!@#$%^&*()[]{}<>.".toCharArray();
+
+        //add numbers to the set
+        int numCount = 3;
+        while (numCount > 0) {
+            int randomNum = rand.nextInt(nums.length);
+            if (!charSet.contains(nums[randomNum])) {
+                charSet.add(nums[randomNum]);
+                numCount--;
+            }
+        }
+
+        //add lowercase letters to set
+        int lowerChars = 5;
+        while (lowerChars > 0) {
+            int randomNum = rand.nextInt(26);
+            if (!charSet.contains(alphabet[randomNum])) {
+                charSet.add(alphabet[randomNum]);
+                lowerChars--;
+            }
+        }
+
+        //add uppercase letters to set
+        int upperChars = 3;
+        while (upperChars > 0) {
+            int randomNum = rand.nextInt(26);
+            if (!charSet.contains(alphabet[randomNum])) {
+                charSet.add(Character.toUpperCase(alphabet[randomNum]));
+                upperChars--;
+            }
+        }
+
+        //add special characters to set
+        int specialChars = 3;
+        while (specialChars > 0) {
+            int randomNum = rand.nextInt(spChars.length);
+            if (!charSet.contains(spChars[randomNum])) {
+                charSet.add(spChars[randomNum]);
+                specialChars--;
+            }
+        }
+
+        String password = "";
+        List<Character> passwordChars = new ArrayList<>(charSet);
+        Collections.shuffle(passwordChars);
+        StringBuilder sb = new StringBuilder();
+        for (Character value : passwordChars) {
+            sb.append(value.toString());
+        }
+        password = sb.toString();
+        return password;
     }
 
     public static void badPassword(String message) {
@@ -91,10 +162,10 @@ public class Main {
                     number = containsNumber(password), specialChar = containsSpecialChar(password);
 
             if (password.equalsIgnoreCase("generate")) {
-                System.out.println("Your randomly generated password is ");
+                System.out.println("Your randomly generated password is " + generatePassword());
                 accepted = true;
             }
-            if (password.length() < 8) {
+            else if (password.length() < 8) {
                 badPassword("Password length is too short.");
                 password = scnr.nextLine();
             } else if (number < 1) {
